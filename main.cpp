2eca7b197b101b87e44f5fa1a3c02d9637ccc3f3
Sanchozz <knopochnik@mail.ru>
2019-05-12 03:35:43 +0300

    Mirror test (doesn't work)

--- main.cpp
+++ main.cpp
@@ -14,6 +14,15 @@
 
 static const GLsizei WIDTH = 640, HEIGHT = 480; //размеры окна
 
+GLuint squareVBO;
+GLuint squareVAO;
+GLuint boxVBO;
+GLuint boxVAO;
+GLuint triangleVBO;
+GLuint triangleVAO;
+GLuint mirrorVBO;
+GLuint mirrorVAO;
+
 int initGL()
 {
     int res = 0;
@@ -96,6 +105,84 @@ void genTriangleBuf(GLuint &vbo, GLuint &vao, GLfloat *pos, GLsizeiptr size)
     glBindVertexArray(0);
 }
 
+void drawScene(GLFWwindow *window, ShaderProgram &program, glm::mat4 view, glm::mat4 projection, bool isReflect)
+{
+    glm::mat4 model(1.0f);
+    program.StartUseShader();                           GL_CHECK_ERRORS;
+
+        // очистка и заполнение экрана цветом
+        //
+        if (isReflect) {
+            int width, height;
+            glfwGetFramebufferSize(window, &width, &height);
+            glViewport(0, 0, width, height);
+            glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+            glClear     (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+        }
+
+        // draw call
+        //
+
+        glBindVertexArray(squareVAO);                                            GL_CHECK_ERRORS;
+        
+        GLfloat cur_time = glfwGetTime();
+
+        program.SetUniform("g_view", view);
+        program.SetUniform("g_projection", projection);
+
+        for (int i = 0; i < 6; i++) {
+            model = glm::mat4(1.0f);
+            model = glm::rotate(model, glm::radians(60.0f * i) + glm::radians(cur_time) * 100.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+            model = glm::translate(model, glm::vec3(2.0f, 2.0f, 0.0f));
+            model = glm::rotate(model, glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+            model = glm::rotate(model, -glm::radians(cur_time) * 180.0f, glm::vec3(0.0f, 0.0f, 1.0f));
+            model = glm::scale(model, glm::vec3(0.75, 0.75, 0.75));
+            program.SetUniform("g_model", model);
+            glDrawArrays(GL_TRIANGLES, 6 * i, 6 * (i + 1));
+        }
+
+        glBindVertexArray(boxVAO);
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(0.0f, 2.0f, 0.0f)); //DELETE
+        model = glm::rotate(model, (GLfloat) glm::radians(cur_time) * 50.0f, glm::vec3(1.0f, 1.0f, 1.0f));
+        program.SetUniform("g_model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        glBindVertexArray(triangleVAO);
+
+        for (int i = 0; i < 6; i++) {
+            model = glm::mat4(1.0f);
+            model = glm::rotate(model, glm::radians(30.0f) + glm::radians(60.0f * i) - glm::radians(cur_time) * 100.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+            model = glm::translate(model, glm::vec3(3.5f, 2.0f, 0.0f));
+            model = glm::rotate(model, glm::radians(60.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+            model = glm::rotate(model, glm::radians(cur_time) * 180.0f, glm::vec3(0.0f, 0.0f, 1.0f));
+            model = glm::scale(model, glm::vec3(0.75, 0.75, 0.75));
+            program.SetUniform("g_model", model);
+            glDrawArrays(GL_TRIANGLES, 0, 6);
+        }
+
+        program.SetUniform("g_model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        program.StopUseShader();
+}
+
+void drawMirror(GLFWwindow *window, ShaderProgram &program, glm::mat4 view, glm::mat4 projection)
+{
+    glm::mat4 model(1.0f);
+    program.StartUseShader();
+        glClearDepth(1.0); 
+        glDepthFunc(GL_ALWAYS);
+        glBindVertexArray(mirrorVAO);
+        program.SetUniform("g_view", view);
+        program.SetUniform("g_projection", projection);
+        program.SetUniform("g_model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDepthFunc(GL_LESS);
+    program.StopUseShader();
+}
+
 
 int main(int argc, char** argv)
 {
@@ -135,10 +222,27 @@ int main(int argc, char** argv)
     shaders[GL_FRAGMENT_SHADER] = "fragment.glsl";
     ShaderProgram program(shaders); GL_CHECK_ERRORS;
 
+    std::unordered_map<GLenum, std::string> shaders_mirror;
+    shaders_mirror[GL_VERTEX_SHADER] = "vertex_mirror.glsl";
+    shaders_mirror[GL_FRAGMENT_SHADER] = "fragment_mirror.glsl";
+    ShaderProgram program_mirror(shaders_mirror); GL_CHECK_ERRORS;
+
     glfwSwapInterval(1); // force 60 frames per second
   
     //Создаем и загружаем геометрию поверхности
     //
+
+    GLfloat mirrorSquarePos[] = 
+    {
+        // Первый треугольник
+        0.5f,  0.0f, 0.5f, 1.0f, 1.0f, 1.0f,  // Верхний правый угол
+        0.5f, 0.0f, -0.5f, 1.0f, 1.0f, 1.0f,// Нижний правый угол
+        -0.5f,  0.0f, 0.5f, 1.0f, 1.0f, 1.0f, 
+        // Второй треугольник
+        0.5f, 0.0f, -0.5f, 1.0f, 1.0f, 1.0f,// Нижний правый угол
+        -0.5f, 0.0f, -0.5f, 1.0f, 1.0f, 1.0f,  // Нижний левый угол
+        -0.5f,  0.0f, 0.5f, 1.0f, 1.0f, 1.0f,// Верхний левый угол
+    };
 
     GLfloat trianglePos[] = 
     {
@@ -261,42 +365,12 @@ int main(int argc, char** argv)
 
 
     glEnable(GL_DEPTH_TEST);
-    GLuint squareVBO;
-    GLuint squareVAO;
+
     genSquareBuf(squareVBO, squareVAO, squarePos, sizeof(squarePos));
-
-    GLuint boxVBO;
-    GLuint boxVAO;
     genBoxBuf(boxVBO, boxVAO, boxPos, sizeof(boxPos));
-
-    GLuint triangleVBO;
-    GLuint triangleVAO;
     genBoxBuf(triangleVBO, triangleVAO, trianglePos, sizeof(trianglePos));
+    genSquareBuf(mirrorVBO, mirrorVAO, mirrorSquarePos, sizeof(mirrorSquarePos));
 
-
-    /*GLuint g_vertexBufferObject;
-    GLuint g_vertexArrayObject;
-    {
-        g_vertexBufferObject = 0;
-        GLuint vertexLocation = 0;
-        GLuint colorLocation = 1; // simple layout, assume have only positions at location = 0
-
-        glGenBuffers(1, &g_vertexBufferObject);                                                        GL_CHECK_ERRORS;
-        glBindBuffer(GL_ARRAY_BUFFER, g_vertexBufferObject);                                           GL_CHECK_ERRORS;
-        glBufferData(GL_ARRAY_BUFFER, sizeof(squarePos), 0, GL_STATIC_DRAW); GL_CHECK_ERRORS;
-        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(squarePos), squarePos);
-
-        glGenVertexArrays(1, &g_vertexArrayObject);                                                    GL_CHECK_ERRORS;
-        glBindVertexArray(g_vertexArrayObject);                                                        GL_CHECK_ERRORS;
-
-       // glBindBuffer(GL_ARRAY_BUFFER, g_vertexBufferObject);                                           GL_CHECK_ERRORS;
-        glEnableVertexAttribArray(vertexLocation);                                                     GL_CHECK_ERRORS;
-        glVertexAttribPointer(vertexLocation, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0); 
-        glEnableVertexAttribArray(colorLocation); 
-        glVertexAttribPointer(colorLocation, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));                           GL_CHECK_ERRORS;
-
-        glBindVertexArray(0);
-    }*/
 
     //цикл обработки сообщений и отрисовки сцены каждый кадр
     while (!glfwWindowShouldClose(window))
@@ -304,72 +378,28 @@ int main(int argc, char** argv)
         glfwPollEvents();
 
         //очищаем экран каждый кадр
-        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);               GL_CHECK_ERRORS;
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); GL_CHECK_ERRORS;
+        //glClearColor(0.1f, 0.1f, 0.1f, 1.0f);               GL_CHECK_ERRORS;
+        //glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); GL_CHECK_ERRORS;
+        glm::mat4 model(1.0f);
 
-        program.StartUseShader();                           GL_CHECK_ERRORS;
-
-        // очистка и заполнение экрана цветом
-        //
-        int width, height;
-        glfwGetFramebufferSize(window, &width, &height);
-        glViewport(0, 0, width, height);
-        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-        glClear     (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
-
-        // draw call
-        //
         glm::mat4 view(1.0f);
         view = glm::translate(view, glm::vec3(0.0f, 0.0f, -8.0f));
         view = glm::rotate(view, glm::radians(20.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 
+        glm::mat4 ref(1.0f);
+        ref[1][1] = -1.0f;
+
+        glm::mat4 reflected_view = view * ref;
+
         glm::mat4 projection(1.0f);
         projection = glm::perspective(45.0f, ((GLfloat) WIDTH) / HEIGHT, 0.1f, 100.0f);
 
-        glm::mat4 model(1.0f);
-
-        glBindVertexArray(squareVAO);                                            GL_CHECK_ERRORS;
+        drawMirror(window, program_mirror, view, projection);
+        drawScene(window, program, reflected_view, projection, true);
+        glClearDepth(0.0f);
+        drawScene(window, program, view, projection, false);
         
-        GLfloat cur_time = glfwGetTime();
-
-        program.SetUniform("g_view", view);
-        program.SetUniform("g_projection", projection);
-
-        for (int i = 0; i < 6; i++) {
-            model = glm::mat4(1.0f);
-            model = glm::rotate(model, glm::radians(60.0f * i) + glm::radians(cur_time) * 100.0f, glm::vec3(0.0f, 1.0f, 0.0f));
-            model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
-            model = glm::rotate(model, glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-            model = glm::rotate(model, -glm::radians(cur_time) * 180.0f, glm::vec3(0.0f, 0.0f, 1.0f));
-            model = glm::scale(model, glm::vec3(0.75, 0.75, 0.75));
-            program.SetUniform("g_model", model);
-            glDrawArrays(GL_TRIANGLES, 6 * i, 6 * (i + 1));
-        }
-
-        glBindVertexArray(boxVAO);
-
-        model = glm::mat4(1.0f);
-        model = glm::rotate(model, (GLfloat) glm::radians(cur_time) * 50.0f, glm::vec3(1.0f, 1.0f, 1.0f));
-        program.SetUniform("g_model", model);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-
-        glBindVertexArray(triangleVAO);
-
-        for (int i = 0; i < 6; i++) {
-            model = glm::mat4(1.0f);
-            model = glm::rotate(model, glm::radians(30.0f) + glm::radians(60.0f * i) - glm::radians(cur_time) * 100.0f, glm::vec3(0.0f, 1.0f, 0.0f));
-            model = glm::translate(model, glm::vec3(3.5f, 0.0f, 0.0f));
-            model = glm::rotate(model, glm::radians(60.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-            model = glm::rotate(model, glm::radians(cur_time) * 180.0f, glm::vec3(0.0f, 0.0f, 1.0f));
-            model = glm::scale(model, glm::vec3(0.75, 0.75, 0.75));
-            program.SetUniform("g_model", model);
-            glDrawArrays(GL_TRIANGLES, 0, 6);
-        }
-
-        program.SetUniform("g_model", model);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-        program.StopUseShader();
+        
         glBindVertexArray(0);
         glfwSwapBuffers(window); 
     }
@@ -384,6 +414,9 @@ int main(int argc, char** argv)
 
     glDeleteVertexArrays(1, &triangleVAO);
     glDeleteBuffers(1,      &triangleVBO);
+
+    glDeleteVertexArrays(1, &mirrorVAO);
+    glDeleteBuffers(1,      &mirrorVBO);
 
     glfwTerminate();
     return 0;
